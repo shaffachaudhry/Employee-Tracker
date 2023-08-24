bca540ed32b76a374d4419209392a8eceac06109
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-08-24 11:37:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'august7',
+    database: 'company_db'
+  },
+  console.log(`Connected to the company_db database.`)
+);
+
+
+// app.use((req, res) => {
+//     res.status(404).end();
+//   });
+  
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
