e5ff1c787b9578708c896afa14e67405340d3d59
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-08-24 12:14:45 -0500

    created db/ connection.js

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
 const inquirer = require("inquirer");
-const mysql = require('mysql2');
+const db = require('./db/connection');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'august7',
-    database: 'company_db'
-  },
-  console.log(`Connected to the company_db database.`)
-);
-
-
-// app.use((req, res) => {
-//     res.status(404).end();
-//   });
-  
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to the company_db database');
+  company_tracker();
+});
